b109c6b337165b54a53b9c1b9f3d75c5056cd49b
neveland98 <54957992+neveland98@users.noreply.github.com>
2020-12-10 15:34:03 -0600

    added equals and hashcode methods for Person class

--- diversity/src/main/java/com/talentpath/diversity/models/Person.java
+++ diversity/src/main/java/com/talentpath/diversity/models/Person.java
@@ -10,6 +10,27 @@ public class Person {
     Race race;
     Party party;
 
+    public Person() {}
+
+    /**
+     * Testing constructor only.
+     * @param firstName First name.
+     * @param lastName Last name.
+     * @param birthYear Birth year.
+     * @param gender Gender.
+     * @param race Race.
+     * @param party Party.
+     */
+    public Person(String firstName,String lastName,Integer birthYear,Gender gender,Race race,Party party) {
+        this.id = -1;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthYear = birthYear;
+        this.gender = gender;
+        this.race = race;
+        this.party = party;
+    }
+
     public Integer getId() {
         return id;
     }
@@ -64,6 +85,32 @@ public class Person {
 
     public void setParty(Party party) {
         this.party = party;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (firstName != null ? !firstName.equals(person.firstName) : person.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(person.lastName) : person.lastName != null) return false;
+        if (birthYear != null ? !birthYear.equals(person.birthYear) : person.birthYear != null) return false;
+        if (gender != person.gender) return false;
+        if (race != person.race) return false;
+        return party == person.party;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName != null ? firstName.hashCode() : 0;
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (birthYear != null ? birthYear.hashCode() : 0);
+        result = 31 * result + (gender != null ? gender.hashCode() : 0);
+        result = 31 * result + (race != null ? race.hashCode() : 0);
+        result = 31 * result + (party != null ? party.hashCode() : 0);
+        return result;
     }
 }
 
