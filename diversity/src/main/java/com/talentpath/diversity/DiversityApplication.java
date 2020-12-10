93d21bf8e5141b50c9b08a65c021ad377c5410cf
neveland98 <54957992+neveland98@users.noreply.github.com>
2020-12-10 13:25:30 -0600

    added SpringBoot project

---
+++ diversity/src/main/java/com/talentpath/diversity/DiversityApplication.java
@@ -0,0 +1,13 @@
+package com.talentpath.diversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiversityApplication.class, args);
+	}
+
+}
---
+++ diversity/src/test/java/com/talentpath/diversity/DiversityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentpath.diversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
