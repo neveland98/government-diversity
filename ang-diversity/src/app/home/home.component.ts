9fc548394ddd6d16901ed63197a623f4338e63bf
jaitken <jaitken97@gmail.com>
2020-12-18 14:13:49 -0500

    more styling to homepage, added container for data dasboard

--- ang-diversity/src/app/home/home.component.ts
+++ ang-diversity/src/app/home/home.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
+  
+  constructor(private renderer: Renderer2) { }
+  
   ngOnInit(): void {
+    this.renderExternalScript('https://public.tableau.com/javascripts/api/viz_v1.js').onload = ()=>{
+    }
+  }
+
+  renderExternalScript(src: string): HTMLScriptElement{
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = src;
+    script.async = true;
+    script.defer = true;
+    this.renderer.appendChild(document.body, script);
+    return script;
   }
 
 }
